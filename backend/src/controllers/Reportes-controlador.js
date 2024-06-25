f3b147fde658eda049f6c781e60a051378452b95
Luis Mario Cruz Hernandez <20221045@uthh.edu.mx>
2024-06-24 18:45:00 -0600

    Envio de correos para casos graves de bullying

--- backend/src/controllers/Reportes-controlador.js
+++ backend/src/controllers/Reportes-controlador.js
@@ -2,8 +2,14 @@ const Accesos = require("../models/Reportes-modelos");
 const { WebhookClient } = require('dialogflow-fulfillment');
 const crypto = require('crypto');
 require('dotenv').config();
+
+const nodemailer = require("nodemailer");
+
 const algorithm = 'aes-256-cbc';
+
 const key = Buffer.from(process.env.ENCRYPTION_KEY, 'hex');
+
+const palabrasArray = ["matar", "suicidar"];
 
 const encrypt = (text) => {
     let iv = crypto.randomBytes(16);
@@ -45,13 +51,13 @@ const RegistrarReportes = async (req, res) => {
             const grupo = agent.parameters.grupo;
             const carrera = agent.parameters.carrera;
             const descripcion = agent.parameters.descripcion;
-            
+
             const { iv: ivNombre, encryptedData: encryptedNombre } = encrypt(nombre);
             const { iv: ivCuatrimestre, encryptedData: encryptedCuatrimestre } = encrypt(cuatrimestre);
             const { iv: ivGrupo, encryptedData: encryptedGrupo } = encrypt(grupo);
             const { iv: ivCarrera, encryptedData: encryptedCarrera } = encrypt(carrera);
             const { iv: ivDescripcion, encryptedData: encryptedDescripcion } = encrypt(descripcion);
-            
+
             const reporte = new Accesos({
                 nombre: encryptedNombre,
                 ivNombre: ivNombre,
@@ -67,6 +73,59 @@ const RegistrarReportes = async (req, res) => {
             });
 
             reporte.save();
+            
+            const desMin = descripcion.toLowerCase();
+            const palabrasDescripcion = desMin.split(/\s+/);
+
+            const palabrasEncontradas = palabrasDescripcion.filter(palabra => palabrasArray.includes(palabra));
+            if (palabrasEncontradas.length > 0) {
+                const html = `
+                        <!DOCTYPE html>
+                        <html lang="en">
+                        <head>
+                            <meta charset="UTF-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                            <title>Restaurar Contraseña</title>
+                            <style>
+                                body {
+                                    font-family: 'Arial', sans-serif;
+                                    background-color: #f4f4f4;
+                                    margin: 0;
+                                    padding: 0;
+                                }
+                                .container {
+                                    max-width: 600px;
+                                    margin: 20px auto;
+                                    background-color: #fff;
+                                    padding: 20px;
+                                    border-radius: 8px;
+                                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                                }
+                                h1 {
+                                    color: #333;
+                                }
+                                p {
+                                    color: #666;
+                                }
+                            </style>
+                        </head>
+                        <body>
+                            <div class="container">
+                                <h1>Caso de bullying grave</h1>
+                                <p>Caso grave de bullying que requiere atencion inmediata</p>
+                                <p>El alumno ${nombre} de la carrera ${carrera} de ${cuatrimestre}-${grupo}
+                                ha estado haciendo bullying a uno de sus compañeros.</p>
+                                <p>El repoprte es el siguiente:  
+                                    ${descripcion}.Por lo cual se requiere de intervencion inmediata.</p>
+                            </div>
+                            
+                        </body>
+                        </html>
+             `
+                const correo = "20221045@uthh.edu.mx"
+                enviarCorreo(correo, "Caso de bullying", html);
+            }
+
             agent.add(`¡Gracias por reportar! Hemos registrado el reporte de ${nombre}.`);
         }
 
@@ -83,10 +142,9 @@ const RegistrarReportes = async (req, res) => {
 
 const Reportes = async (req, res) => {
     try {
-        
+
         const reportes = await Accesos.find();
 
-       
         const reportesDesencriptados = reportes.map((reporte) => {
             try {
                 const nombreDesencriptado = decrypt(reporte.ivNombre, reporte.nombre);
@@ -94,9 +152,10 @@ const Reportes = async (req, res) => {
                 const grupoDesencriptado = decrypt(reporte.ivGrupo, reporte.grupo);
                 const carreraDesencriptado = decrypt(reporte.ivCarrera, reporte.carrera);
                 const descripcionDesencriptada = decrypt(reporte.ivDescripcion, reporte.descripcion);
-                
+
+
                 return {
-                    fecha:reporte.toObject().fecha,
+                    fecha: reporte.toObject().fecha,
                     nombre: nombreDesencriptado,
                     cuatrimestre: cuatrimestreDesencriptado,
                     grupo: grupoDesencriptado,
@@ -122,6 +181,37 @@ const Reportes = async (req, res) => {
         res.status(500).send('Error al obtener los reportes');
     }
 };
+
+
+const enviarCorreo = (correo, subject, html) => {
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'chatfriend375@gmail.com',
+            pass: 'eznj whwf skut bwqe'
+        }
+    });
+
+    var mailOptions = {
+        from: 'chatfriend375@gmail.com',
+        to: correo,
+        subject: subject,
+        html: html
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            res.send({ status: 'success' });
+        }
+    });
+}
+
+
+
+
+
 
 module.exports = {
     RegistrarReportes,
