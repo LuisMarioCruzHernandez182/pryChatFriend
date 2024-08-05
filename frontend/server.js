94f2e91a5e8974521f2e24bd09eaf446acde8712
Luis Mario Cruz Hernandez <20221045@uthh.edu.mx>
2024-08-05 13:49:51 -0600

    Configuracion para render con express

---
+++ frontend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware para servir archivos estáticos desde la carpeta "dist"
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Ruta catch-all para manejar la navegación del frontend en una SPA
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
